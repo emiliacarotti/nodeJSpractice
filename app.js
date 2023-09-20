40bbcab50bf3e2009c4f8655fe919d8ea2924852
emiliacarotti <emiliac@vt.edu>
2023-09-19 21:12:14 -0400

    230919-packages

--- app.js
+++ app.js
@@ -1,68 +1,14 @@
-// import http module
+// import http core global module
 const http = require('http');
 
-// import file system module
-const fs = require('fs');
+// ./ is local path, not global
+const routes = require('./routes');
 
 // createServer callback function stored in const server runs whenever request hits server, returns a response
-// edit console.log to change what you want to receive as a response in your terminal
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-
-    // technically you can send response back from the server this way, this is here for learning purposes, not usually done this way in real webs
-
-    // parse url as constant
-    const url = req.url;
-    const method = req.method;
-
-    // if at home page, create form which takes input and redirect to /message page on click
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html');
-        return res.end();
-
-    }
-
-    // if at /message page AND method is POST, create file with user inputted text from form
-    // statusCode 302 is a redirect code
-    if (url === '/message' && method === 'POST') {
-        // req.on to listen for data, receive chunk and perform function as specified below
-        // node.js will perform the below until all chunks received and pushed to body array
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        // after we load the data (sent in thru the form with the button), we need to parse it
-        // we create a buffer called parsedBody and all the chunks from inside the body to it
-        // it appears as 'message=...' in your terminal when the page is run
-        // then we split that message and take the right side of the '=' and save it into const message
-        // now we put fs.writeFileSync inside the function for it to run after all this and actually store that message into our text file
-        // if we have something that depends on the incoming data we need to move it also inside the event listener too so that we ensure it runs after it
-        // otherwise fs.writeFileSync will run immediately and it won't load correctly, it'll run before all this happens, node.js doesn't wait
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody)
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-
-        })
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
-    res.write('</html');
-    res.end();
-});
+// requestHandler method is imported from routes.js to this file and stored in routes constant (above)
+// routes constant holds requesHandler function and it can be used here as a handler
+// we are telling program to execute the function stored in routes for incoming requests
+const server = http.createServer(routes);
 
 // node js will keep server running to listen for incoming requests. in prod you remove 3000, that is local host for testing
 server.listen(3000);
